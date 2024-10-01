84c58cf76dcfd34bce8b6dff753002232c8bf106
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2024-10-01 19:20:12 +0200

    modified:   home.css 	modified:   home.html 	modified:   home.js

--- home.js
+++ home.js
@@ -1,57 +1,21 @@
-let display = document.getElementsByClassName(display);
-
-let answer;
-let clicked_1 = [];
-let clicked_2 = [];
-let invert_clicked = false
+let display = document.getElementById('display');
 
 function clicked(number) {
-    if(!invert_clicked)
-        clicked_1.push(number)
-    else
-        clicked_2.push(number)
+    display.value += number;
 }
 
 function operator(op) {
-    clicked_1.push(op);
-    invert_clicked = !invert_clicked;
-}
-
-function ac() {
-    clicked_1 = [];
-    clicked_2 = [];
-    invert_clicked = false
-}
-
-function del() {
-    if(!invert_clicked)
-        clicked_1.pop();
-    else
-        clicked_2.pop();
+    display.value += op;
 }
 
 function is_equal_to() {
-    switch(clicked_1[clicked_1.length - 1]) {
-        case 1:
-        clicked_1.pop();
-        answer = clicked_1 + clicked_2;
-        case 2:
-        clicked_1.pop();
-        answer = clicked_1 - clicked_2;
-        case 3:
-        clicked_1.pop();
-        answer = clicked_1 * clicked_2;
-        case 4:
-        clicked_1.pop();
-        answer = clicked_1 / clicked_2;
-    }
-
-    ac();
+    display.value = eval(display.value)
 }
 
-function ans() {
-    if(!invert_clicked)
-        clicked_1 = answer;
-    else
-        clicked_2 = answer;
+function ac() {
+    display.value = '';
+}
+
+function del() {
+    display.value = display.value.slice(0, -1);
 }
